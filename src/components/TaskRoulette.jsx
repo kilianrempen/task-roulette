56a9e6f22e0d17a714864c4819c9fc467da3b5a2
Kilian Rempen <kilianrempen@gmail.com>
2026-01-31 13:23:10 -0700

    test github action

--- src/components/TaskRoulette.jsx
+++ src/components/TaskRoulette.jsx
@@ -166,7 +166,7 @@ const TaskRoulette = () => {
         ) : (
             <>
               <h2 className="flex items-center justify-center gap-3 text-2xl font-bold mb-4 text-center">
-                Add tasks to the list and decide how long you'll work.</h2>
+                POOOOOOP!!!Add tasks to the list and decide how long you'll work.</h2>
               <h3 className="text-center text-gray-400 mb-4 text-lg ">Spin the wheel to let fate decide what you'll do!</h3>
             </>
         )}
